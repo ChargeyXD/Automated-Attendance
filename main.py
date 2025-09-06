76b50aa2b4f4523da2115acf251c78f25e2d8bf5
Rehan <chargedxgaming@gmail.com>
2025-09-06 14:16:20 +0530

    relocated html

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "hello there"
-
+    return render_template('index.html')
 @app.route('/video')
-def camera_feed():
-    return fc.Webcam()
+def video():
+    return "video"
 
 if __name__ == "__main__":
     app.run(debug=True)
