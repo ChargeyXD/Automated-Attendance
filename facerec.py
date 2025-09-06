87c1daf2c5fac8eccc716d43e58b4c74fcd5ac01
Rehan <chargedxgaming@gmail.com>
2025-09-06 16:19:16 +0530

    gitignore

--- facerec.py
+++ facerec.py
@@ -1,6 +1,10 @@
 import cv2 as cv
 
 vid = cv.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 680)
+vid.set(3, 1280)
+vid.set(4, 680)
 
+while True:
+    success, img = vid.read()
+    cv.imshow("Webcam", img)
+    cv.waitKey(1)
