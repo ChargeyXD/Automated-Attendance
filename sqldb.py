908bc159cca6c27de24167e8b9328dc7ffbeef96
Jaswanth <jaswanth.s239@gmail.com>
2025-09-07 15:41:13 +0530

    test

--- sqldb.py
+++ sqldb.py
@@ -1,13 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from database import db
-
-db = SQLAlchemy()
-
-class User(db.Model):
-    __tablename__ = "users"
+from flask import Flask
 
 
-    id = db.column(db.integer,primary_Key = True)
-    name = db.column(db.String(100),
-    nullable=False)
+    
